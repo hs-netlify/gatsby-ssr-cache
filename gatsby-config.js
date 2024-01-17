affbeab1272f775c3405b832323c69164b1062dd
Henry Smith <henry.smith@netlify.com>
2024-01-17 15:26:17 +0000

    Feat: Update server data time

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         { key: "Cache-Control", value: "public, max-age=0, must-revalidate" },
         {
           key: "Netlify-CDN-Cache-Control",
-          value: "public, max-age=0, stale-while-revalidate=3600",
+          value: "public, max-age=60, stale-while-revalidate=3600",
         },
         {
           key: "Test",
